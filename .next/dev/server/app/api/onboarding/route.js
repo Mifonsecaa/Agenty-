3a6376c4d625f7a0c1ceed641711bd46e71d7a65
Miguel Fonseca <mifonsecaa@unal.edu.co>
2026-03-04 22:03:06 -0500

    Remove .next from tracking

--- .next/dev/server/app/api/onboarding/route.js
+++
@@ -1,8 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/onboarding/route.js")
-R.c("server/chunks/node_modules_next_01ed99b8._.js")
-R.c("server/chunks/node_modules_openai_5c1656a1._.js")
-R.c("server/chunks/node_modules_zod_abea3340._.js")
-R.c("server/chunks/[root-of-the-server]__8db0347b._.js")
-R.c("server/chunks/_next-internal_server_app_api_onboarding_route_actions_fbcdf9b4.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/onboarding/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/onboarding/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
